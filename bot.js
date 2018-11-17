1f8f462455b13b62bbe392aaab42ee247368055b
Panz155 <41524573+Panz155@users.noreply.github.com>
2018-11-17 20:11:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`  سلام  `,'https://www.twitch.tv/MeeRcY')
+    client.user.setGame(`  Panz155 is here  `,'https://www.twitch.tv/MeeRcY')
 
 });
 
