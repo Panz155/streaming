1e26c2a15ffbbc45406a7b5778738d05a807aed5
Panz155 <41524573+Panz155@users.noreply.github.com>
2018-11-17 16:13:17 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`AFK`,'https://www.twitch.tv/MeeRcY')
+    client.user.setGame(`i'm here my boy`,'https://www.twitch.tv/MeeRcY')
 
 });
 
