5b42eab3c35c6e6458f5577287a096c36aa70593
Panz155 <41524573+Panz155@users.noreply.github.com>
2018-11-17 17:09:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`i'm here my boy`,'https://www.twitch.tv/MeeRcY')
+    client.user.setGame(`النصاب بضل نصاب `,'https://www.twitch.tv/MeeRcY')
 
 });
 
