3de290ed4c4827c0a595d2b78594d4d703dbdee1
Panz155 <41524573+Panz155@users.noreply.github.com>
2018-11-20 14:35:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 client.on('ready', () => {
-    client.user.setGame(`  BUY MVP+ FOR CREDITS  `,'https://www.twitch.tv/MeeRcY')
+    client.user.setGame(`  https://discord.gg/ZmmsvYp  `,'https://www.twitch.tv/MeeRcY')
 
 });
 
